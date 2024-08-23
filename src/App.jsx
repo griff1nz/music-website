9e85c69ac6d73dd7bd18a61481e7c59aef2d36a0
Zion G <griff1nz@hell-yea.local>
2024-08-22 23:14:21 -0400

    Stuck footer to bottom

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,22 @@ function App() {
 
   return (
     <>
-      <Header />
-      <div className='background'>
-        <h2 style={{ textAlign: 'center', paddingTop: '50px' }}>Online Lessons for All Ages</h2>
-        <Button id='button' href='https://calendly.com/zion-isaac-griffin' target='_blank' variant="primary" size="lg">
-          Book a Lesson!
-        </Button>
+      <div style={{ minHeight: '93vh' }}>
+        <Header />
+        <div className='background'>
+          <h2 style={{ textAlign: 'center', paddingTop: '50px' }}>Online Lessons for All Ages</h2>
+          <Button id='button' href='https://calendly.com/zion-isaac-griffin' target='_blank' variant="primary" size="lg">
+            Book a Lesson!
+          </Button>
 
-      </div>
-      <div id='sections'>
-        <About />
-        <Pricing />
-        <Lessons />
-        <Contact />
+        </div>
+        <div id='sections'>
+          <About />
+          <Pricing />
+          <p>pp</p>
+          <Lessons />
+          <Contact />
+        </div>
       </div>
       <Footer />
     </>
