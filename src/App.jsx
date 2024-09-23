a44c02e673fcbff0ad9fe89e16707a3056a86fcb
Zion G <defendingmymaininbox@gmail.com>
2024-09-23 16:02:22 -0400

    Added my intro video to the site

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ function App() {
       <div style={{ minHeight: '93vh' }}>
         <Header />
         <div className='background'>
-          <div style={{ paddingTop: '50px'}}>
-            <motion.h2 style={{ color: 'white', textAlign: 'center', backgroundImage: 'linear-gradient(to bottom right, dimgray, gainsboro', border: '5px solid white', borderRadius: '8px', marginLeft: '25%', marginRight: '25%', padding: '10px'}}
+          <div style={{ paddingTop: '50px' }}>
+            <motion.h2 style={{ color: 'white', textAlign: 'center', backgroundImage: 'linear-gradient(to bottom right, dimgray, gainsboro', border: '5px solid white', borderRadius: '8px', marginLeft: '25%', marginRight: '25%', padding: '10px' }}
               variants={{
                 hidden: { opacity: 0, y: 75 },
                 visible: { opacity: 1, y: 0 },
@@ -25,7 +25,10 @@ function App() {
               initial="hidden"
               animate="visible"
               transition={{ duration: 0.5, delay: 0.25 }}
-            >Online Saxophone and Cello Lessons for East Pennsylvania and Beyond</motion.h2>
+            >Interactive Online Saxophone & Cello Lessons, Anytime, Anywhere</motion.h2>
+          </div>
+          <div style={{ display: 'flex' }}>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/QXFK3wiEm8Y?si=bl6enjYkewQAk9a8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen style={{ marginLeft: 'auto', marginRight: "auto", marginTop:'10px', height: '25vh', width: '430px'}}></iframe>
           </div>
           <div>
             <Button id='button' href='https://calendly.com/zion-isaac-griffin' target='_blank' variant="secondary" size="lg">
